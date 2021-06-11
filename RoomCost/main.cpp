8faa79aea2a36b6154aec435f017f9b6aafdb594
Alex <barankaslon@hotmail.com>
2021-06-10 22:08:23 -0300

    first variables

--- RoomCost/main.cpp
+++ RoomCost/main.cpp
@@ -4,6 +4,13 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+int numberOfRooms;
+int pricePerRoom;
+int cost;
+int totalCost;
+float Tax;
+const int valid = 30;
+
 int main() {
-	cout << "Hey" << endl;
+	cout << "Hello, welcome to Gvozd Carpet Cleaning Service (GCCS)" << endl;
 }
