47a36b1dd9386950af6e38c41a308e4fbd03788a
Alex <barankaslon@hotmail.com>
2021-07-05 14:30:21 -0300

    constructor overload

--- oop/overload/overload/main.cpp
+++ oop/overload/overload/main.cpp
@@ -17,6 +17,45 @@ double Sum(double a, double b)
 	return a + b;
 }
 
+
+class Point
+{
+private:
+	int x;
+	int y;
+public:
+
+	Point()
+	{
+		x = {};
+		y = {};
+	}
+
+	Point(int valueX, int valueY)
+	{
+		x = valueX;
+		y = valueY;
+	}
+
+	Point(int valueX, bool boolean)
+	{
+		x = valueX;
+		if (boolean)
+		{
+			y = 1;
+		}
+		else
+		{
+			y = -1;
+		}
+	}
+
+	void Print()
+	{
+		cout << "Value X: " << x << "\tValue Y: " << y << endl;
+	}
+};
+
 int main()
 {
 	cout << Sum(5, 6) << endl;
@@ -24,4 +63,15 @@ int main()
 	cout << Sum(4.1, 5.2) << endl;
 
 	cout << Sum(3, 6, 8) << endl;
+
+	Point a;
+	a.Print();
+
+	Point b(5, 14);
+	b.Print();
+
+	Point c(7, false);
+	c.Print();
+
+	return 0;
 }
